23073ae70d145c4ffbb50809e76d174d99c579c3
fengguo118 <fengguo118@gmail.com>
2015-05-06 13:03:26 +0800

    update new code

--- static/admin/js/controllers.js
+++ static/admin/js/controllers.js
@@ -1,5 +1,17 @@
 var loginModule = angular.module("loginModule", []);
 
 loginModule.controller('loginCtroller', function($scope, $http, $location, $state, $stateParams){
-	
+	$scope.login = function(userInfo){
+		console.log(userInfo);
+		$http.post('/login', userInfo).success(function(data){
+			console.log(data);
+			if (data.status == 200)
+			{
+				window.session = data;
+				$location.path('/main');
+			}
+		}).error(function(error){
+			console.log(error);
+		});
+	}
 });
